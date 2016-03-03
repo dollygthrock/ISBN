6df20538517ff02804d61e1fd3e05ff9459b1d93
dollygthrock <dollygthrock@gmail.com>
2016-03-02 21:46:21 -0500

    Refactoring of code

--- ISBN.rb
+++ ISBN.rb
@@ -1,7 +1,15 @@
-def valid_isbn?(isbn_string)
+def delete_spaces_and_hyphens(isbn_string)
 	string_without_spaces = isbn_string.delete(" ")
 	string_without_spaces_or_hyphens = string_without_spaces.delete("-")
+end
+
+
+
+
+def valid_isbn?(isbn_string)
+	string_without_spaces_or_hyphens = delete_spaces_and_hyphens(isbn_string)
 	string_without_spaces_or_hyphens.length == 10
 end
+
 
 
