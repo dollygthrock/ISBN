f57dd80ecd6757da1d19147e552c3306869ab2f6
dollygthrock <dollygthrock@gmail.com>
2016-03-02 20:47:24 -0500

    From Wednesday session

--- ISBN.rb
+++ ISBN.rb
@@ -1,10 +1,5 @@
-def generate_valid_isbn(isbn)
-	isbn.split(",").map{|s|s.to_i}
-	if isbn.length == 10
-	puts isbn
-	else
-	end
+def valid_isbn?(isbn_string)
+	false
 end
 
 
-generate_valid_isbn(1,2,3,4,5,6,7,8,9,0)
--- ISBN.test.rb
+++ ISBN.test.rb
@@ -2,8 +2,10 @@ require "minitest/autorun"
 require_relative "ISBN.rb"
 
 class TestISBN < Minitest::Test
-	def ISBN_only_valid_with_ten_or_thirteen_numbers
-	assert_equal(false,generate_valid_isbn.length == 9)
-	assert_equal(false,generate_valid_isbn.length == 11)
+	def test_string_without_10_or_13_numbers
+	assert_equal(false,valid_isbn?(""))
+	assert_equal(false,valid_isbn?("123456789123"))
+
 	end
+
 end 
