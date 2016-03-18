e5eeb05d0fb6f5b6c2a955481cd7e03a0d62b516
dollygthrock <dollygthrock@gmail.com>
2016-03-18 16:17:37 -0400

    Reads data from excel file

--- isbn_data.rb
+++ isbn_data.rb
@@ -1,6 +1,7 @@
-require "isbn_kata.rb"    
+require 'csv' 
 
-isbn_numbers = CSV.read('input_isbn_file.csv')
-CSV.foreach('input_isbn_file.csv') do #go through each item in the spreadsheet
-CSV.parse(File.read('input_isbn_file.csv'))#read: from a file; parse: from a string object
- end
+input = CSV.read('input_isbn_file.csv')
+CSV.foreach('input_isbn_file.csv') do |row|
+	puts row.inspect
+CSV.parse(File.read('input_isbn_file.csv'))
+end
