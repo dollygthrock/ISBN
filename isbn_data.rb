b73c43e4a43464f643de9b82f2ca690e37d6cbb3
dollygthrock <dollygthrock@gmail.com>
2016-03-19 12:29:07 -0400

    Code to run the valid/invalids

--- isbn_data.rb
+++ isbn_data.rb
@@ -1,7 +1,16 @@
+require_relative "isbn_kata.rb"
 require 'csv' 
 
-input = CSV.read('input_isbn_file.csv')
+file = File.open("duplicate_isbn_examples.csv", "w")
 CSV.foreach('input_isbn_file.csv') do |row|
-	puts row.inspect
-CSV.parse(File.read('input_isbn_file.csv'))
+	if row[0] == "ITEM"
+		isbn_check = "CHECKS"
+	elsif valid_isbn(row[1]) == true
+		isbn_check = "VALID"
+	else
+		isbn_check = "INVALID"
+	end
+	file << row[0] + ", " + row[1]+ ", " + isbn_check + "\n"
+		
 end
+file.close
